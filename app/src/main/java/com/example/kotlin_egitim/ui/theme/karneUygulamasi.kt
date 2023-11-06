01f5918c5f9addb4820af74aa4c31a730954d4b4
Berkay Veysel Ayköse <berkay.ayk@hotmail.com>
2023-11-06 16:31:24 +0300

    For ve diziler ile kullanicidan not alma

---
+++ app/src/main/java/com/example/kotlin_egitim/ui/theme/karneUygulamasi.kt
@@ -0,0 +1,29 @@
+package com.example.kotlin_egitim.ui.theme
+
+import java.util.Scanner
+
+fun main()
+{
+    val dersler = Array<String>(5) {""}//{"","","","",""}
+    val notlar = Array<Int>(5) {0}//{0,0,0,0,0}
+
+    val girdi = Scanner(System.`in`)
+
+    //count() dizi kaç elamlı bulmaya yarar
+    for (i in 0  until dersler.count())//0,1,2,3,4
+    {
+        println("${i+1}. dersin ismini giriniz")
+        val ders = girdi.next()
+        dersler[i] = ders
+
+        println("${i+1}. dersin notunu giriniz")
+        val not = girdi.nextInt()
+        notlar[i] = not
+    }
+
+    for(i in 0 until notlar.count())
+    {
+        println("${dersler[i]} : ${notlar[i]}")
+    }
+
+}
